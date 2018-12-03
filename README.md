5e212f737c6df7504626874fa7822a0203e141c8
uremember809 <34588627+uremember809@users.noreply.github.com>
2018-12-02 23:12:21 -0500

    a little detail

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # small tools collection
+
+Some tools I created to handle some task for myself.
+
+I will try to keep add and maintain them.
---
+++ small tools.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[28]:
+
+
+import os
+import re
+
+
+# In[35]:
+
+def remove_extension(ext = 'txt'):
+
+    """ this script will remove the specific extension of all the files in the current folder
+    and list the content of the folder if success"""
+    
+    file_list = os.listdir()
+
+    file_list_splitted = []
+    for file in file_list:
+        file_list_splitted.append(file.split('.'))    # split the file name.
+
+    for i, s in enumerate(file_list_splitted):
+        if s[-1] == ext:
+            s.pop()
+        file_list_splitted[i] = '.'.join(s)           # remove the extension and join the file name back
+
+    zipped = zip(file_list, file_list_splitted)
+
+    for o, n in zipped:
+        os.rename(o, n)
+    
+    os.listdir()
+
+
+# In[41]:
+
+def zero_one():
+    
+    """ changing the leading 1, 2, 3... in the file name to 01, 02, 03... for the sorting purpose."""
+    
+    file_list = os.listdir()
+
+    for file in file_list:
+        file_new = re.sub('^(\d) ', r'0\1 ', file)
+        os.rename(file, file_new)
+
+    os.listdir()
+
+
+# In[ ]:
+
+
+
+
